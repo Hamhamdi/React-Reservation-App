e8e4572f74bb72ed36004b6ada97c7c601b0434a
hamza <hamdihamza.aui@gmail.com>
2022-06-29 16:04:55 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
+const app = express();
+dotenv.config();
+
+const connect = async () =>{
+
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongodb")
+    } catch (error) {
+        throw error
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mogodb disconnected");
+});
+
+// mongoose.connection.on("connected", () => {
+//     console.log("mogodb connected");
+// });
+//    This code is only for explaining api concept
+// app.get("/users", (req, res) => {
+//     res.send("hello first request !");
+// });
+
+// Middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+
+app.listen(8800, ()=>{
+    connect();
+    console.log("Connected to backend.");
+});
